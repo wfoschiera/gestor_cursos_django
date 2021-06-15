b9b277867e5e455e6cc4543594f8e9321e0963f5
wfoschiera <wfoschiera@gmail.com>
2021-06-15 11:11:22 -0400

    included test_sentry_debug
    close #36

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,6 +1,12 @@
 from django.test import Client
+import pytest
 
 
 def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
+
+
+def test_sentry_debug(client: Client):
+    with pytest.raises(ZeroDivisionError):
+        client.get('/sentry-debug/')
