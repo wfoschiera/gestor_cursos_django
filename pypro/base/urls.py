b7b18b321576ab25ff2c71f7aacff960b5ed7b82
wfoschiera <wfoschiera@gmail.com>
2021-06-22 11:22:05 -0400

    first commit
    closed #50

---
+++ pypro/base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from pypro.base.views import home, trigger_error_sentry
+
+
+app_name = 'base'
+urlpatterns = [
+    path('', home, name='home'),
+    path('sentry-debug/', trigger_error_sentry),
+]
