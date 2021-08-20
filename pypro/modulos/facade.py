ed0c0f64b2f5cfb79c2b0bab2d9b55d3d5cb0403
William Foschiera <wfoschiera@gmail.com>
2021-08-20 16:57:07 -0400

    criada view para modulos e gerenciador de contexto

---
+++ pypro/modulos/facade.py
@@ -0,0 +1,9 @@
+from pypro.modulos.models import Modulo
+from typing import List
+
+
+def listar_modulos_ordenados() -> List[Modulo]:
+    """
+    Lista módulos ordenados por títulos
+    """
+    return list(Modulo.objects.order_by("titulo").all())
--- pypro/settings.py
+++ pypro/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "cloudinary",
     "pypro.base",
     "pypro.aperitivos",
+    "pypro.modulos",
 ]
 
 ROOT_URLCONF = "pypro.urls"
@@ -69,6 +70,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "pypro.modulos.context_processors.listar_modulos",
             ],
         },
     },
