d12049ce92797db31fd4143d50379ec993c86c74
William Foschiera <wfoschiera@gmail.com>
2021-08-20 15:07:22 -0400

    fixed Black and Flake8 runners

--- pypro/aperitivos/admin.py
+++ pypro/aperitivos/admin.py
@@ -5,6 +5,6 @@ from pypro.aperitivos.models import Video
 
 @register(Video)
 class VideoAdmin(ModelAdmin):
-    list_display = ('titulo', 'slug', 'creation', 'vimeo_id')
-    ordering = ('creation',)
-    prepopulated_fields = {'slug': ('titulo',)}  # aceita varios campos para criar a slug, por isso uma tupla
+    list_display = ("titulo", "slug", "creation", "vimeo_id")
+    ordering = ("creation",)
+    prepopulated_fields = {"slug": ("titulo",)}  # aceita varios campos para criar a slug, por isso uma tupla
