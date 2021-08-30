8d1f4e38b65cca427b218b7367a64ab6f488325a
wfoschiera <wfoschiera@gmail.com>
2021-08-30 10:31:58 -0400

    criada aba de modulos, fixada versao python para 3.8.6 (fix heroku)

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from ordered_model.models import OrderedModel
 
 from django.utils.translation import gettext_lazy as _
@@ -9,12 +10,16 @@ from django.utils.translation import gettext_lazy as _
 
 class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
-    publico = models.TextField(null=True)
-    descricao = models.TextField(null=True)
-    order = models.PositiveIntegerField(_("order"), editable=False, db_index=True, null=True)
+    publico = models.TextField()
+    descricao = models.TextField()
+    order = models.PositiveIntegerField(_("order"), editable=False, db_index=True)
+    slug = models.SlugField()
 
     class Meta(OrderedModel.Meta):
         pass
 
     def __str__(self):
         return self.titulo
+
+    def get_absolute_url(self):
+        return reverse("modulos:detalhe", kwargs={"slug": self.slug})
