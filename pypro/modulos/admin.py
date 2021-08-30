a57290b082be96181fdf5246385c108597bf49fb
wfoschiera <wfoschiera@gmail.com>
2021-08-30 10:31:58 -0400

    criada aba de modulos, fixada versao python para 3.8.6 (fix heroku)

--- pypro/modulos/admin.py
+++ pypro/modulos/admin.py
@@ -8,3 +8,4 @@ from pypro.modulos.models import Modulo
 @admin.register(Modulo)
 class ModuloAdmin(OrderedModelAdmin):
     list_display = ("titulo", "publico", "move_up_down_links")
+    prepopulated_fields = {"slug": ("titulo",)}  # aceita varios campos para criar a slug, por isso uma tupla
